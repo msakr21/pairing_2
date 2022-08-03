a019a1afc031782713a9bbff6a91ca879398cbdc
Mostafa Sakr <msakr3@gmail.com>
2022-08-03 16:17:08 -0600

    Add places

--- friends.rb
+++ friends.rb
@@ -2,3 +2,8 @@ arrays = ["a", "b", "c", "d", "e"]
 puts arrays [2]
 puts arrays [4]
 puts arrays [0]
+
+places_to_visit = ["Ireland", "Egypt", "Japan", "Romania", "Canada"]
+
+places_to_visit.unshift ("Taiwan")
+puts places_to_visit
